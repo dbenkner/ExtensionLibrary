3a2cff297c4dd4f834df5c3eacb7692c601a590e
David Benkner <d.benkner@gmail.com>
2023-08-23 15:52:33 -0400

    Add project files.

---
+++ ExtensionLibrary/StringExtensions.cs
@@ -0,0 +1,39 @@
+﻿namespace ExtensionLibrary
+{
+    public static class StringExtensions
+    {
+        public static decimal ToDecimal(this string str)
+        {
+            var s = str;
+            s = s.Trim();
+            if (s[0] == '$')
+            {
+                s = s.Substring(1);
+            }
+            s = s.Replace(",", "");
+            decimal nbr;
+            var rc = decimal.TryParse(s, out nbr);
+            if(!rc)
+            {
+                throw new Exception("string contains non numeric charachters");
+            }
+            return nbr;
+        }
+        public static string AllCaps(this string str)
+        {
+            return str.ToUpper();
+        }
+        public static string AllSmall(this string str)
+        {
+            return str.ToLower();
+        }
+        public static void WriteLine(this string str)
+        {
+            Console.WriteLine(str);
+        }
+            public static int Eval(this int x)
+            {
+                return (x * x) + (3 * x) + 5;
+            }
+    }
+}
---
+++ TestExtensionLIbarary/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using ExtensionLibrary;
+
+var s = "   $123,456.78";
+var d = "$ 432,432,122.54";
+
+Console.WriteLine($"{s} as a deciaml is {s.ToDecimal():C}");
+Console.WriteLine($"{d} as a deciaml is {d.ToDecimal():C}");
+
+s.WriteLine();
+var e = "This is a string";
+e.WriteLine();
+
+int a =4;
+Console.WriteLine(a.Eval());
+
+
+/*
+var s = "sadKSfasFDSdfs";
+
+var sAllCaps = s.AllCaps();
+var sAllSmall = sAllCaps.AllSmall();
+
+Console.WriteLine($"{s} as all caps is {sAllCaps} and all small is {sAllSmall}");
+*/
